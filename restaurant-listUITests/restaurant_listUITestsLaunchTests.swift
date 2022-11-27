a80555551571183c471d723766e84f8f834e2cdf
Guadalupe Vlček <gvlcek@icloud.com>
2022-11-27 14:37:10 -0300

    Initial Commit

---
+++ restaurant-listUITests/restaurant_listUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  restaurant_listUITestsLaunchTests.swift
+//  restaurant-listUITests
+//
+//  Created by Guadalupe Vlček on 27/11/2022.
+//
+
+import XCTest
+
+final class restaurant_listUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
